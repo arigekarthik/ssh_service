18c9cee03486ccf52b55e479d6e57d39509e8d9a
arigekarthik <arigekarthik@gmail.com>
2020-09-05 16:31:45 +0530

    post method

--- server.py
+++ server.py
@@ -5,20 +5,20 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# @app.route('/list_dir', methods=['GET'])
-# def list_remote_dir():
-#     sftp_conn = create_sftp_connection()
-#     directory_structure = sftp_conn.listdir_attr()
-#     dir_contents = []
-#     for attr in directory_structure:
-#         dir_contents.append([attr.filename, attr])
-#     sftp_conn.close()
-#     response = app.response_class(
-#         response=json.dumps(str(dir_contents)),
-#         status=200,
-#         mimetype='application/json'
-#     )
-#     return response
+@app.route('/list_dir', methods=['GET'])
+def list_remote_dir():
+    sftp_conn = create_sftp_connection()
+    directory_structure = sftp_conn.listdir_attr()
+    dir_contents = []
+    for attr in directory_structure:
+        dir_contents.append([attr.filename, attr])
+    sftp_conn.close()
+    response = app.response_class(
+        response=json.dumps(str(dir_contents)),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 @app.route('/upload', methods = ['POST'])
 def upload_to_remote():
@@ -40,17 +40,14 @@ def upload_to_remote():
 @app.route('/upload_from_string', methods = ['POST'])
 def upload_to_remote_v2():
     body = request.get_json()
-    print(body)
     with open("sftp_out.txt", "w") as text_file:
         text_file.write(body['file'])
     sftp_conn = create_sftp_connection()
     localFilePath = f"./sftp_out.txt"
     remoteFilePath = f"sftp_out.txt"
-    print(os.listdir('.'), localFilePath, remoteFilePath)
     sftp_conn.put(localFilePath, remoteFilePath)
     sftp_conn.close()
     os.remove("sftp_out.txt")
-    print(os.listdir('.'))
     response = app.response_class(
         response=json.dumps({}),
         status=200,
