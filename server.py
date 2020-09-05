4c61ba7f8884c1cd715676e09624e822b9877e7c
arigekarthik <arigekarthik@gmail.com>
2020-09-05 15:01:48 +0530

    post method

--- server.py
+++ server.py
@@ -18,14 +18,25 @@ def list_remote_dir():
 def upload_to_remote():
     f = request.files['file']
     f.save(f.filename)
-    arr = os.listdir('.')
-    print(arr)
+    sftp_conn = create_sftp_connection()
+    localFilePath = f"./{f.filename}"
+    remoteFilePath = f"./{f.filename}"
+    sftp_conn.put(localFilePath, remoteFilePath)
+    sftp_conn.close()
     response = app.response_class(
         response=json.dumps({}),
         status=200,
         mimetype='application/json'
     )
     return response
+
+def create_sftp_connection():
+    myHostname = "test.rebex.net"
+    myUsername = "demo"
+    myPassword = "password"
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    return pysftp.Connection(host=myHostname, username=myUsername, password=myPassword, cnopts=cnopts)
 
 def connect_and_list():
     myHostname = "test.rebex.net"
